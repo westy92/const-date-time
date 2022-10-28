cc6de332b9c1022b3e606a722dc79ff68e36efe2
Seth Westphal <westy92@gmail.com>
2022-10-27 21:49:38 -0500

    Improve and validate example.

--- example/const_date_time_example.dart
+++ example/const_date_time_example.dart
@@ -1,5 +1,11 @@
 import 'package:const_date_time/const_date_time.dart';
 
 void main() {
-  final constDateTime = ConstDateTime(0);
+  final constDateTime = const ConstDateTime(0);
+  // you can pass a ConstDateTime where a DateTime is expected!
+  printDate(constDateTime);
+}
+
+void printDate(DateTime dateTime) {
+  print(dateTime.toIso8601String());
 }

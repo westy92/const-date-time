637a002daaafdd00bbff09c241960579a427f8e2
Seth Westphal <westy92@gmail.com>
2022-10-27 22:45:13 -0500

    Multiple constructors.

--- test/const_date_time_test.dart
+++ test/const_date_time_test.dart
@@ -4,17 +4,17 @@ import 'package:test/test.dart';
 void main() {
   group('Test methods', () {
     test('operator ==', () {
-      var constDateTime = ConstDateTime(0);
+      var constDateTime = ConstDateTime.fromMillisecondsSinceEpoch(0);
       var dateTime = DateTime.fromMillisecondsSinceEpoch(0);
       expect(constDateTime == dateTime, isTrue);
       expect(dateTime == constDateTime, isTrue);
 
-      constDateTime = ConstDateTime(0, isUtc: true);
+      constDateTime = ConstDateTime.fromMillisecondsSinceEpoch(0, isUtc: true);
       dateTime = DateTime.fromMillisecondsSinceEpoch(0, isUtc: true);
       expect(constDateTime == dateTime, isTrue);
       expect(dateTime == constDateTime, isTrue);
 
-      constDateTime = ConstDateTime(0);
+      constDateTime = ConstDateTime.fromMillisecondsSinceEpoch(0);
       dateTime = DateTime.fromMillisecondsSinceEpoch(123);
       expect(constDateTime == dateTime, isFalse);
       expect(dateTime == constDateTime, isFalse);
@@ -23,15 +23,15 @@ void main() {
 
   group('Test getters', () {
     test('hashCode', () {
-      var constDateTime = ConstDateTime(0);
+      var constDateTime = ConstDateTime.fromMillisecondsSinceEpoch(0);
       var dateTime = DateTime.fromMillisecondsSinceEpoch(0);
       expect(constDateTime.hashCode == dateTime.hashCode, isTrue);
 
-      constDateTime = ConstDateTime(0, isUtc: true);
+      constDateTime = ConstDateTime.fromMillisecondsSinceEpoch(0, isUtc: true);
       dateTime = DateTime.fromMillisecondsSinceEpoch(0, isUtc: true);
       expect(constDateTime.hashCode == dateTime.hashCode, isTrue);
 
-      constDateTime = ConstDateTime(0);
+      constDateTime = ConstDateTime.fromMillisecondsSinceEpoch(0);
       dateTime = DateTime.fromMillisecondsSinceEpoch(123);
       expect(constDateTime.hashCode == dateTime.hashCode, isFalse);
     });
